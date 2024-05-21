e470c837845c87097776fe096991915342864f5f
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-20 21:23:19 -0600

    Create ciclofor2.java

---
+++ ciclofor2.java
@@ -0,0 +1,11 @@
+public class ciclofor2 {
+    public static void main(String[] args) {
+        int longitudSerie = 50;
+        int i;
+        for(i = 1;  i<=(longitudSerie/2); i++){
+            System.out.println((2*i));
+            System.out.println((3*i));
+        }
+    }
+    
+}
